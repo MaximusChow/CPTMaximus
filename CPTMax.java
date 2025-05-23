3c483ee3aac0dbb0c1c6743c8325bee401ae449f
MaximusChow <maximus.chow27@ycdsbk12.ca>
2025-05-23 09:40:14 -0400

    Testing a string

--- CPTMax.java
+++ CPTMax.java
@@ -4,5 +4,14 @@ public class CPTMax{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("Hello World");
+		
+		String strWord;
+		
+		con.println("Give me a word");
+		strWord = con.readLine();
+		
+		if(strWord.length() > 0){
+			con.println("Nice!");
+		}
 	}
 }
