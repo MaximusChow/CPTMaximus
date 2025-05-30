5b7973bcb15ce497d29565722176b0e72d0dfc59
MaximusChow <maximus.chow27@ycdsbk12.ca>
2025-05-30 09:42:50 -0400

    Started lose condition page
    when points decrease to zero it clears the screen
    
    still figuring out the main game
    may need to organize and start over with simpler codes

--- CPTMax.java
+++ CPTMax.java
@@ -5,15 +5,16 @@ public class CPTMax{
 		Console con = new Console(1280, 720);
 		
 		String strGuess;
-		String strWord = "bread";
+		String strWord = "cat";
 		int intLength;
 		int intCount;
 		boolean blnCorrectGuess;
-		
+		int intUnderscores;
 		
 		
 		intLength = strWord.length();
 		int intPoints = intLength;
+		intUnderscores = intLength;
 		System.out.println("There are "+intPoints+" remaining letters to be guessed");
 		
 		String strLetter[];
@@ -28,39 +29,59 @@ public class CPTMax{
 		
 		
 		for(intCount = 1; intCount <= intLength; intCount++){
-			con.print("_ ");
+			con.print("_");
 			con.sleep(100);
 		}
 		
 		while(intPoints != 0){
-			con.println("\n\nYou have "+intPoints+" points.");
+			con.println("\nYou have "+intPoints+" points.");
 			con.println("Guess a letter");
 			strGuess = con.readLine();
 			
+			
 			for(intCount = 0; intCount < intLength; intCount++){
-				if(strGuess.equalsIgnoreCase(strLetter[intCount])){
-					con.println("You correctly guessed a letter!");
+				if(strGuess.equals(strLetter[intCount])){
 					strGuessed[intCount] = strGuess;
+					
 					blnCorrectGuess = true;
+					
+				}else{
+					
+					
 				}
 			}
 			
-			if(blnCorrectGuess = false){
-				con.println("Wrong guess.");
-				intPoints--;
-			}
-			//print out the letters that were guessed using both arrays
-			//con.print("_");
-			//con.print(strLetter[intCount]);
-			
-			//for(intCount = 0; intCount < intLength; intCount++){
-				if(strGuessed[0] == (strLetter[0])){
-					con.print(strLetter[0]);
+			for(intCount = 0; intCount < intLength; intCount++){
+				if(strGuessed[intCount] == strLetter[intCount]){
+					con.print(strGuessed[intCount]);
+				}else{
+					con.print("_");
 				}
 				
-			//}
+			}
+			
+			
+			if(intUnderscores == intLength){
+				con.println("\nYou guessed a correct letter!");
+				intUnderscores = intUnderscores -1;
+				con.println("You have "+intUnderscores+" letters left to guess.");
+				
+				
+			}else{
+				con.println("\nThat was not one of the letters.");
+				con.println("You have "+intUnderscores+" letters left to guess.");
+				intPoints = intPoints - 1;
+				
+				
+			}
+			
 			
 		}
+		
+		con.clear();
+		con.println("You Lose!");
+		con.println("Points: "+intPoints);
+		
 	}
 }		
 
